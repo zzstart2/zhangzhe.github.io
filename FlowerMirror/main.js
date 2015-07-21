aafca697d96039c97a63bb78613ba4bc5b36c2ac
张哲 <zhangzhe19960115@gmail.com>
2015-07-21 22:02:20 +0800

    real final

--- FlowerMirror/main.js
+++ FlowerMirror/main.js
@@ -10,6 +10,7 @@ var speed = 800;
 var movespeed = 20;
 var messageList = $('#messages');
 var numofPlayed = 0;
+var sh;
 
 var mid=new Array();
 mid[1] = "piano/raw/c4.ogg";
@@ -26,87 +27,93 @@ document.getElementById("loginNameInput").setAttribute("value", localusername);
 
 function init(){
   pointID = 0;
-  magicFlag = 1;
-  $(".centerCircle").css({"background-image": "url(src/centerCircleBlue.png)"});
   score = 0;
   gameEnd = 1;
   speed = 800;
   movespeed = 20;
+  document.getElementById("score").setAttribute("value", "0 点");
 };
 
 $("#plus").slideUp();
 
-//生成小花朵
-function createPoint(){
-if(gameEnd === 0){
-  var smallPoint = document.createElement("div");
-  smallPoint.setAttribute("class", "smallPoint");
+function begin(){
+  init();
+  gameEnd = 0;
+  sh = window.setInterval(function createPoint(){
+  console.log("asdfghhj");
+  if(gameEnd === 0){
+    var smallPoint = document.createElement("div");
+    smallPoint.setAttribute("class", "smallPoint");
 
-  //随机一种花的颜色
-  var pointType = Math.round(Math.random());
-  if(pointType === 0){
-    //console.log("000"+pointType);
-    smallPoint.setAttribute("typeID", "0");
-    smallPoint.id="pointRed";
-  }else{
-    //console.log(pointType);
-    smallPoint.setAttribute("typeID", "1");
-    smallPoint.id="pointBlue";
-  };
-
-  //将花朵随机放置在外围圆圈上
-  var x;
-  var y;
-  var angle = 2*Math.PI*Math.random();
-  x = $("#outerCircle").offset().left + 300 + 250*(Math.cos(angle));
-  y = $("#outerCircle").offset().top + 300 + 250*(Math.sin(angle));
-  //console.log(x);
-  //console.log(y);
-
-  $(".outerCircle").append(smallPoint);
-  smallPoint.style.left = (x - 15) + "px";
-  smallPoint.style.top = (y - 15) + "px";
-
-  if(movespeed > 3){
-    movespeed = movespeed-(score/20);
-  }
-
-  //控制花朵运动向中心
-  var pointsh = window.setInterval(function(){
-    if(gameEnd === 0){
-      distance2 = getDistanceToCenter(x, y, 30, 30);
-      if(distance2 > 800){
-        x-=2*Math.cos(angle);
-        y-=2*Math.sin(angle);
-        smallPoint.style.left = (x - 15) + "px";
-        smallPoint.style.top = (y - 15) + "px";
-      }else{//到达中心后判断是否终止游戏
-        if(smallPoint.getAttribute("typeID") === ""+magicFlag){//得分
-          score ++;
-          document.getElementById("score").setAttribute("value", score+" 点");
-          //console.log(score);
-          pointID ++;
-          smallPoint.setAttribute("typeID", "2");
-        }
-        else if(smallPoint.getAttribute("typeID") === "0" || smallPoint.getAttribute("typeID") === "1"){//游戏终止
-          pointID ++;
-          smallPoint.setAttribute("typeID", "2");
-          alert("end!");
-          var message = messageField.val();
-          messagesRef.push({name:localusername, score:message});
-          init();
-          clearInterval(sh);
-        }
-        smallPoint.remove();
-        clearInterval(pointsh);
-      }
+    //随机一种花的颜色
+    var pointType = Math.round(Math.random());
+    if(pointType === 0){
+      //console.log("000"+pointType);
+      smallPoint.setAttribute("typeID", "0");
+      smallPoint.id="pointRed";
     }else{
-      smallPoint.remove();
-      clearInterval(pointsh);
+      //console.log(pointType);
+      smallPoint.setAttribute("typeID", "1");
+      smallPoint.id="pointBlue";
+    };
+
+    //将花朵随机放置在外围圆圈上
+    var x;
+    var y;
+    var angle = 2*Math.PI*Math.random();
+    x = $("#outerCircle").offset().left + 300 + 250*(Math.cos(angle));
+    y = $("#outerCircle").offset().top + 300 + 250*(Math.sin(angle));
+    //console.log(x);
+    //console.log(y);
+
+    $(".outerCircle").append(smallPoint);
+    smallPoint.style.left = (x - 15) + "px";
+    smallPoint.style.top = (y - 15) + "px";
+
+    if(movespeed > 3){
+      movespeed = movespeed-(score/20);
     }
-  }, movespeed);
-}
+
+    //控制花朵运动向中心
+    var pointsh = window.setInterval(function(){
+      if(gameEnd === 0){
+        distance2 = getDistanceToCenter(x, y, 30, 30);
+        if(distance2 > 800){
+          x-=2*Math.cos(angle);
+          y-=2*Math.sin(angle);
+          smallPoint.style.left = (x - 15) + "px";
+          smallPoint.style.top = (y - 15) + "px";
+        }else{//到达中心后判断是否终止游戏
+          if(smallPoint.getAttribute("typeID") === ""+magicFlag){//得分
+            score ++;
+            document.getElementById("score").setAttribute("value", score+" 点");
+            //console.log(score);
+            pointID ++;
+            smallPoint.setAttribute("typeID", "2");
+          }
+          else if(smallPoint.getAttribute("typeID") === "0" || smallPoint.getAttribute("typeID") === "1"){//游戏终止
+            pointID ++;
+            smallPoint.setAttribute("typeID", "2");
+            alert("end!");
+            var message = messageField.val();
+            messagesRef.push({name:localusername, score:message});
+            init();
+            sh = window.clearInterval(sh);
+          }
+          smallPoint.remove();
+          pointsh = window.clearInterval(pointsh);
+        }
+      }else{
+        smallPoint.remove();
+        pointsh = clearInterval(pointsh);
+      }
+    }, movespeed);
+  }
+}, speed);
 };
+
+//生成小花朵
+
 
 function getDistanceToCenter(x, y, dx, dy){
   var cx = x + dx/2 - $("#outerCircle").offset().left;
@@ -116,7 +123,9 @@ function getDistanceToCenter(x, y, dx, dy){
 };
 
 $("#start").on('click', function(e){
-  location.reload(false);
+  gameEnd = 0;
+  $('#signinBase').remove();
+  begin();
 });
 
 $("#plus").on('click', function(e){
@@ -131,19 +140,23 @@ $(".scorePic").on('click', function(e){
 
 $("#startButton").on('click', function(e){
   gameEnd = 0;
-  window.setInterval(createPoint, speed);
+  begin();
   $('#signinBase').remove();
-  $(document).keydown(function (e){
-    if (e.keyCode === 32){
-      if(magicFlag === 0){
-        magicFlag = 1;
-        $(".centerCircle").css({"background-image": "url(src/centerCircleBlue.png)"});
-      }else{
-        magicFlag = 0;
-        $(".centerCircle").css({"background-image": "url(src/centerCircleRed.png)"});
-      }
+});
+
+$(window).keydown(function (e){
+  if (e.keyCode === 32){
+    console.log("space");
+    if(magicFlag === 0){
+      magicFlag = 1;
+      $(".centerCircle").css({"background-image": "url(src/centerCircleBlue.png)"});
+      console.log(magicFlag);
+    }else if(magicFlag === 1){
+      magicFlag = 0;
+      $(".centerCircle").css({"background-image": "url(src/centerCircleRed.png)"});
+      console.log(magicFlag);
     }
-  });
+  }
 });
 
 messagesRef.on("value", function(snapshot) {
